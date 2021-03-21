4d1562698f97d716dc9637deda7ee6adc00fb043
Lokesh Patel <32188450+lokesh58@users.noreply.github.com>
2021-03-21 22:11:46 +0530

    Added custom command prefix support

--- src/events/misc/command-handler.js
+++ src/events/misc/command-handler.js
@@ -1,8 +1,33 @@
+const cmdPrefixSchema = require('../../models/command-prefix-schema')
+const {prefixes} = require('../../collections')
+
+const getPrefix = async (guildID) => {
+    let prefix = prefixes.get(guildID)
+    if (!prefix) {
+        let result = await cmdPrefixSchema.findById(guildID).catch(console.error)
+        if (!result) {
+            // Prefix not in database, store the default one
+            result = await new cmdPrefixSchema({
+                _id: guildID,
+                prefix: process.env.DEFAULT_PREFIX
+            }).save().catch(console.error)
+            if (!result) {
+                // There was some error in writing to databse, abort
+                return
+            }
+        }
+        // Now we have the prefix, store in collection
+        prefixes.set(guildID, result.prefix)
+        prefix = result.prefix
+    }
+    return prefix
+}
+
 module.exports = (client) => {
-    client.on('message', (message) => {
+    client.on('message', async (message) => {
         if (message.author.bot || message.channel.type === 'dm') return
-        const prefix = process.env.DEFAULT_PREFIX
-        //TODO: implement different prefix across servers using mongodb
+        const prefix = await getPrefix(message.guild.id)
+        if(!prefix) return //Some error retriving prefix, abort command execution
         if(!message.content.startsWith(prefix)) return
         const args = message.content.slice(prefix.length).trim().split(/\s+/)
         const cmdName = args.shift().toLowerCase()
