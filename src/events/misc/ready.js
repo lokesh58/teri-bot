32986ffcfdcb8e13f6ba397ee6460449ea5ce656
Lokesh Patel <32188450+lokesh58@users.noreply.github.com>
2021-03-21 20:16:01 +0530

    Added login to mongodb

--- src/events/misc/ready.js
+++ src/events/misc/ready.js
@@ -1,5 +1,13 @@
+const mongoose = require('mongoose')
+
 module.exports = (client) => {
     client.on('ready', () => {
+        mongoose.connect(process.env.MONGODB_URI, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true
+        }).then(
+            console.log('Connected to mongodb!')
+        ).catch(console.error)
         console.log(`${client.user.username} is online!`)
     })
 }
