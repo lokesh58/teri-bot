bafae494166d348da0307d5d75830588b0541016
Lokesh Patel <32188450+lokesh58@users.noreply.github.com>
2021-03-21 17:23:59 +0530

    Added utility to start listening for events

---
+++ src/utils/event-starter.js
@@ -0,0 +1,17 @@
+const {readdirSync, lstatSync} = require('fs')
+const {join} = require('path')
+
+module.exports = (client) => {
+    const start = (dir) => {
+        const files = readdirSync(join(__dirname, dir))
+        for (const file of files) {
+            const stat = lstatSync(join(__dirname, dir, file))
+            if (stat.isDirectory()) {
+                start(join(dir, file))
+            } else if (file.endsWith('.js')) {
+                const event = require(join(__dirname, dir, file))
+                event(client)
+            }
+        }
+    }
+}
