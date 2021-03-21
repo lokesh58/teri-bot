f490ccb7040b82ea09113196a1edb01ec6936f19
Lokesh Patel <32188450+lokesh58@users.noreply.github.com>
2021-03-21 19:00:55 +0530

    Replying to user instead of a normal message

--- src/commands/misc/ping.js
+++ src/commands/misc/ping.js
@@ -2,7 +2,7 @@ module.exports = {
     name: 'ping',
     desc: 'Returns the average ping to the bot\'s websocket',
     run: (message, args) => {
-        const {channel, client} = message
-        channel.send(`The average ping to websocket is ${client.ws.ping}`).catch(console.error)
+        const {client} = message
+        message.reply(`The average ping to websocket is ${client.ws.ping}ms`).catch(console.error)
     }
 }
