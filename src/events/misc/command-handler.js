9c4b20d14140b153b935b17c6c07948f854c4979
Lokesh Patel <32188450+lokesh58@users.noreply.github.com>
2021-03-21 21:03:41 +0530

    Updated to use aliases

--- src/events/misc/command-handler.js
+++ src/events/misc/command-handler.js
@@ -1,12 +1,12 @@
 module.exports = (client) => {
     client.on('message', (message) => {
+        if (message.author.bot || message.channel.type === 'dm') return
         const prefix = process.env.DEFAULT_PREFIX
         //TODO: implement different prefix across servers using mongodb
-        if (message.author.bot || message.channel.type === 'dm') return
         if(!message.content.startsWith(prefix)) return
         const args = message.content.slice(prefix.length).trim().split(/\s+/)
         const cmdName = args.shift().toLowerCase()
-        const cmd = client.commands.get(cmdName)
+        const cmd = client.commands.get(cmdName) || client.commands.get(client.aliases.get(cmd))
         if(cmd) {
             console.log(`Running command ${cmd.name}`)
             cmd.run(message, args)
