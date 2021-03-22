8fed4ba96204874b30819e1491ad2b78539149ed
Lokesh Patel <32188450+lokesh58@users.noreply.github.com>
2021-03-22 11:38:53 +0530

    Added category field

--- src/commands/misc/ping.js
+++ src/commands/misc/ping.js
@@ -1,6 +1,7 @@
 module.exports = {
     name: 'ping',
     desc: 'Returns the average ping to the bot\'s websocket',
+    category: 'misc',
     run: (message, args) => {
         const {client} = message
         message.reply(`The average ping to websocket is ${client.ws.ping}ms`).catch(console.error)
