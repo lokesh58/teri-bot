ba127acb520591ab4571eaeb457cd02d0139a086
Lokesh Patel <32188450+lokesh58@users.noreply.github.com>
2021-03-22 20:55:01 +0530

    Added specific command help

--- src/commands/misc/help.js
+++ src/commands/misc/help.js
@@ -53,10 +53,57 @@ const listCommands = (message) => {
     channel.send(embed).catch(console.error)
 }
 
+/**
+ * 
+ * @param {Message} message 
+ * @param {String} cmdName 
+ */
+const commandHelp = (message, cmdName) => {
+    const {client, channel, guild, author} = message
+    const cmd = client.commands.get(cmdName) || client.commands.get(client.aliases.get(cmdName))
+    const prefix = prefixes.get(guild.id)
+    if (!prefix) {
+        prefix = process.env.DEFAULT_PREFIX
+        console.error('Executing help command without prefix in collection')
+    }
+    if (!cmd) {
+        const embed = new MessageEmbed()
+                            .setTitle(`Command \`${cmdName}\` not found`)
+                            .setDescription(`Please use \`${prefix}help\` to get list of all commands`)
+                            .setColor('FF0000')
+        message.reply(embed).catch(console.error)
+    } else {
+        const embed = new MessageEmbed()
+                            .setTitle(`Command Help for \`${cmd.name}\``)
+                            .setDescription(
+                                cmd.expectedArgs
+                                    ? `\`${prefix}${cmd.name} ${cmd.expectedArgs}\``
+                                    : `\`${prefix}${cmd.name}\``
+
+                            ).setColor('RANDOM')
+                            .setFooter(
+                                `Requested by ${author.tag}`,
+                                author.displayAvatarURL({dynamic: true})
+                            ).setTimestamp()
+        if (cmd.aliases && Array.isArray(cmd.aliases)) {
+            embed.addField('Aliases', `\`${cmd.aliases.join('\`, \`')}\``)
+        }
+        if (cmd.desc) {
+            embed.addField('Description', cmd.desc)
+        }
+        if (cmd.parameters) {
+            embed.addField('Parameters', cmd.parameters)
+        }
+        channel.send(embed).catch(console.error)
+    }
+}
+
 module.exports = {
     name: 'help',
+    aliases: ['h'],
     desc: 'Gives the list of commands or help regarding a specific command',
     expectedArgs: '(<command name>)',
+    parameters: `\`(<command name>)\`: Optional, replace with an available command`,
     category: 'misc',
     /**
      * 
@@ -65,11 +112,9 @@ module.exports = {
      */
     run: (message, args) => {
         if (args.length === 0) {
-            //TODO: Display the list of commands
             listCommands(message)
         } else {
-            //TODO: Display help of cmd at args[0]
-            message.reply('Under Development').category(console.error)
+            commandHelp(message, args[0].toLowerCase())
         }
     }
 }
