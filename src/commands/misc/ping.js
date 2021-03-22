8b4e0ab3fa541dc006f67ffb684c68eaf8ac2218
Lokesh Patel <32188450+lokesh58@users.noreply.github.com>
2021-03-22 17:15:10 +0530

    Added parameter types for easier understanding

--- src/commands/misc/ping.js
+++ src/commands/misc/ping.js
@@ -1,7 +1,14 @@
+const {Message} = require('discord.js')
+
 module.exports = {
     name: 'ping',
     desc: 'Returns the average ping to the bot\'s websocket',
     category: 'misc',
+    /**
+     * 
+     * @param {Message} message 
+     * @param {[String]} args 
+     */
     run: (message, args) => {
         const {client} = message
         message.reply(`The average ping to websocket is ${client.ws.ping}ms`).catch(console.error)
