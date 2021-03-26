5b9361f4d662bf19d9be82b21bdc1c5bd8326a06
Lokesh Patel <32188450+lokesh58@users.noreply.github.com>
2021-03-26 13:58:32 +0530

    Utility for checking user permissions for running command

---
+++ src/utils/checkPermission.js
@@ -0,0 +1,21 @@
+module.exports = (member, cmd) => {
+    const {client} = member
+    //Check if bot owner only command
+    if (cmd.ownerOnly) {
+        if (!client.owners
+            || !Array.isArray(client.owners)
+            || !client.owners.includes(member.id)) {
+            return false
+        }
+    }
+    //Check if user has the required permissions to use this command
+    const reqPerm = cmd.requiredPermissions
+    if(reqPerm && Array.isArray(reqPerm)) {
+        console.log(`Checking user permissions for running command ${cmd.name}`)
+        for(const perm of reqPerm) {
+            if (!member.hasPermission(perm))
+                return false
+        }
+    }
+    return true
+}
