3155722545a9de379490fa4b73f79b7e90cb759e
Lokesh Patel <32188450+lokesh58@users.noreply.github.com>
2021-03-26 13:58:58 +0530

    Created a utility for getting prefix

---
+++ src/utils/getPrefix.js
@@ -0,0 +1,24 @@
+const {prefixes} = require('../collections')
+const cmdPrefixSchema = require('../models/command-prefix-schema')
+
+module.exports = async (guildID) => {
+    let prefix = prefixes.get(guildID)
+    if (!prefix) {
+        let result = await cmdPrefixSchema.findById(guildID).catch(console.error)
+        if (!result) {
+            // Prefix not in database, store the default one
+            result = await new cmdPrefixSchema({
+                _id: guildID,
+                prefix: process.env.DEFAULT_PREFIX
+            }).save().catch(console.error)
+            if (!result) {
+                // There was some error in writing to databse, abort
+                return
+            }
+        }
+        // Now we have the prefix, store in collection
+        prefixes.set(guildID, result.prefix)
+        prefix = result.prefix
+    }
+    return prefix
+}
