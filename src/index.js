fa1bf09e590ae885e3a9cb9f01f6e1d6c4ea5056
Lokesh Patel <32188450+lokesh58@users.noreply.github.com>
2021-03-26 13:57:49 +0530

    Added array containing ID of owner for owner only commands

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ const cmdLoader = require('./utils/command-loader')
 const eventStarter = require('./utils/event-starter')
 
 client = new Client()
+client.owners = [
+    '651011187076759553',
+    '322078119018364938'
+]
 cmdLoader(client)
 eventStarter(client)
 client.login(process.env.BOT_TOKEN)
