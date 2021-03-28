8795f282e6098dd889648906de67bf3aaae39213
Lokesh Patel <32188450+lokesh58@users.noreply.github.com>
2021-03-28 22:34:41 +0530

    Added command to view all natures stored in database

---
+++ src/commands/Honkai Impact 3/Valks/nature.js
@@ -0,0 +1,39 @@
+const {Message, MessageEmbed} = require('discord.js')
+const natureSchema = require('../../../models/Honkai Impact 3/nature-schema')
+
+module.exports = {
+    name: 'nature',
+    desc: 'Gives the list of all valkyrja natures stored in the database',
+    category: 'Honkai Impact 3',
+    /**
+     * 
+     * @param {Message} message 
+     * @param {[String]} args 
+     */
+    run: async (message, args) => {
+        const natures = await natureSchema.find({}).catch(console.error)
+        if (!natures) {
+            message.reply('Some error occured. Please try again.').catch(console.error)
+            return
+        }
+        let list = ''
+        if(natures.length === 0) {
+            list = 'No valkyrja nature has been added yet'
+        } else {
+            for (const nature of natures) {
+                if (list.length > 0) list += '\n'
+                list += `• \`${nature.name}\` ${nature.emoji}`
+            }
+        }
+        const {author, channel} = message
+        const embed = new MessageEmbed()
+                            .setTitle('List of Valkyrja Natures')
+                            .setDescription(list)
+                            .setColor('RANDOM')
+                            .setFooter(
+                                `Requested by ${author.tag}`,
+                                author.displayAvatarURL({dynamic: true})
+                            ).setTimestamp()
+        channel.send(embed).catch(console.error)
+    }
+}
