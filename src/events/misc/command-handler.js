a25e56dc34cfc5c2e07b265dfc2849808d87f2d3
Lokesh Patel <32188450+lokesh58@users.noreply.github.com>
2021-03-29 14:02:47 +0530

    Added escape sequence such that emotes are read correctly

--- src/events/misc/command-handler.js
+++ src/events/misc/command-handler.js
@@ -8,7 +8,8 @@ module.exports = (client) => {
         const prefix = await getPrefix(message.guild.id)
         if(!prefix) return //Some error retriving prefix, abort command execution
         if(!message.content.toLowerCase().startsWith(prefix)) return
-        const args = parse(message.content.slice(prefix.length).trim())
+        //replace < with \< and > with \> because they are special characters for parse, hence need escape characters
+        const args = parse(message.content.slice(prefix.length).trim().replace('<','\\<').replace('>', '\\>'))
         const cmdName = args.shift().toLowerCase()
         const cmd = client.commands.get(cmdName) || client.commands.get(client.aliases.get(cmdName))
         if(cmd) {
