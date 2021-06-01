c9690867436fd8a4c8b6f5b002e9745479020cc9
Lokesh Patel <32188450+lokesh58@users.noreply.github.com>
2021-06-01 09:41:03 +0530

    Added more detailed translation results

--- src/commands/utility/translate.js
+++ src/commands/utility/translate.js
@@ -1,5 +1,6 @@
-const {Message} = require('discord.js')
+const {Message, MessageEmbed} = require('discord.js')
 const translator = require('@iamtraction/google-translate')
+const ISO6391 = require('iso-639-1')
 
 module.exports = {
     name: 'translate',
@@ -18,12 +19,25 @@ module.exports = {
             message.reply('Please speicify the text to translate!').catch(console.error)
         }else{
             const text = args.join(' ')
-            const tltext = await translator(text, {to:'en'}).catch(console.error)
-            if(!tltext){
-                message.reply('Some error occurred. Please try again!').catch(console.error)
-            }else{
-                message.reply(tltext.text).catch(console.error)
+            const res = await translator(text, {to:'en'}).catch(console.error)
+            if(!res){
+                return message.reply('Some error occurred. Please try again!').catch(console.error)
             }
+            const {channel, author} = message
+            const embed = new MessageEmbed()
+                                .setTitle('Translation Results')
+                                .setDescription(
+                                    `**Detected Language**: ${ISO6391.getName(res.from.language.iso)}`
+                                ).addField('Translated From',
+                                    res.from.text.value || text
+                                ).addField('Translation Result', res.text)
+                                .setColor('RANDOM')
+                                .setFooter(
+                                    `Requested by ${author.tag}`,
+                                    author.displayAvatarURL({dynamic: true})
+                                )
+                                .setTimestamp()
+            channel.send(embed).catch(console.error)
         }
     }
 }
