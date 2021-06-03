a68bc5a31f47a4859f59b0daef5a47a4d8b90ce1
Lokesh Patel <32188450+lokesh58@users.noreply.github.com>
2021-06-03 18:54:56 +0530

    Added new command greyscale

---
+++ src/commands/Image Manipulation/greyscale.js
@@ -0,0 +1,42 @@
+const {Message, MessageAttachment} = require('discord.js')
+const canvacord = require('canvacord')
+
+module.exports = {
+    name: 'greyscale',
+    aliases: ['grayscale'],
+    desc: 'Grayscale a mentioned user',
+    expectedArgs: '<@user>',
+    parameters: `\`<@user>\`: mention or username or discord ID of the user\n**Note:** If the user is not specified or not found, then the author of message is triggered`,
+    category: 'Image Manipulation',
+    /**
+     * 
+     * @param {Message} message 
+     * @param {[String]} args 
+     */
+    run: async (message, args) => {
+        let user = message.mentions.users.first()
+        if(!user){
+            if(args.length > 0) {
+                const u = args.join(' ').toLowerCase()
+                user = message.client.users.resolve(u)
+                if(!user){
+                    user = message.client.users.cache.find(
+                        us => us.username.toLowerCase() === u
+                                || us.tag.toLowerCase() === u
+                    )
+                }
+            }
+        }
+        if(!user){
+            user = message.author
+        }
+        const avatar = user.displayAvatarURL({format: 'png'})
+        const grey = await canvacord.Canvas.greyscale(avatar).catch(console.error)
+        message.channel.send(
+            new MessageAttachment(
+                grey,
+                `${user.username}_greyscale.png`
+            )
+        ).catch(console.error)
+    }
+}
