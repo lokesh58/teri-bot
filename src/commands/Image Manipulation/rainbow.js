bd7399afe59ec55c893e6877325825034e9d739e
Lokesh Patel <32188450+lokesh58@users.noreply.github.com>
2021-06-03 19:37:31 +0530

    Added new command rainbow

---
+++ src/commands/Image Manipulation/rainbow.js
@@ -0,0 +1,41 @@
+const {Message, MessageAttachment} = require('discord.js')
+const canvacord = require('canvacord')
+
+module.exports = {
+    name: 'rainbow',
+    desc: 'Rainbow a mentioned user',
+    expectedArgs: '<@user>',
+    parameters: `\`<@user>\`: mention or username or discord ID of the user\n**Note:** If the user is not specified or not found, then the author of message is used`,
+    category: 'Image Manipulation',
+    /**
+     * 
+     * @param {Message} message 
+     * @param {[String]} args 
+     */
+    run: async (message, args) => {
+        let user = message.mentions.users.first()
+        if(!user){
+            if(args.length > 0) {
+                const u = args.join(' ').toLowerCase()
+                user = message.client.users.resolve(u)
+                if(!user){
+                    user = message.client.users.cache.find(
+                        us => us.username.toLowerCase() === u
+                                || us.tag.toLowerCase() === u
+                    )
+                }
+            }
+        }
+        if(!user){
+            user = message.author
+        }
+        const avatar = user.displayAvatarURL({format: 'png'})
+        const rainbow = await canvacord.Canvas.rainbow(avatar).catch(console.error)
+        message.channel.send(
+            new MessageAttachment(
+                rainbow,
+                `${user.username}_rainbow.png`
+            )
+        ).catch(console.error)
+    }
+}
