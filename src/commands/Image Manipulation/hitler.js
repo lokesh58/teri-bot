b5ad7a7df53dd4709e2081cf52ec3398c30274fe
Lokesh Patel <32188450+lokesh58@users.noreply.github.com>
2021-06-03 18:56:37 +0530

    Added new command hitler

---
+++ src/commands/Image Manipulation/hitler.js
@@ -0,0 +1,41 @@
+const {Message, MessageAttachment} = require('discord.js')
+const canvacord = require('canvacord')
+
+module.exports = {
+    name: 'hitler',
+    desc: 'Worse than hitler a mentioned user',
+    expectedArgs: '<@user>',
+    parameters: `\`<@user>\`: mention or username or discord ID of the user\n**Note:** If the user is not specified or not found, then the author of message is triggered`,
+    category: 'Image Manipulation',
+    /**
+     * 
+     * @param {Message} message 
+     * @param {[String]} args 
+     */
+    run: async (message, args) => {
+        let user = message.mentions.users.first()
+        if(!user){
+            if(args.length > 0) {
+                const u = args.join(' ').toLowerCase()
+                user = message.client.users.resolve(u)
+                if(!user){
+                    user = message.client.users.cache.find(
+                        us => us.username.toLowerCase() === u
+                                || us.tag.toLowerCase() === u
+                    )
+                }
+            }
+        }
+        if(!user){
+            user = message.author
+        }
+        const avatar = user.displayAvatarURL({format: 'png'})
+        const hitler = await canvacord.Canvas.hitler(avatar).catch(console.error)
+        message.channel.send(
+            new MessageAttachment(
+                hitler,
+                `${user.username}_hitler.png`
+            )
+        ).catch(console.error)
+    }
+}
