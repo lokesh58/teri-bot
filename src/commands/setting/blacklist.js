8d753c839bc6342ce56804f7c578df19af1339cb
Lokesh Patel <32188450+lokesh58@users.noreply.github.com>
2021-06-05 20:51:01 +0530

    Fixed bug where same words were added to list multiple times

--- src/commands/setting/blacklist.js
+++ src/commands/setting/blacklist.js
@@ -27,17 +27,21 @@ const updateBlacklist = async (guildId, blist) => {
 const addWords = async (message, words) => {
     const {guild, channel, author} = message
     const blist = await getBlacklist(guild.id)
-    words = words.filter(w => blist.indexOf(w) < 0) //Remove duplicate words
-    if(!updateBlacklist(guild.id, blist.concat(words))){
+    const toAdd = []
+    for(const word of words){
+        if(blist.includes(word) || toAdd.includes(word)) continue
+        toAdd.push(word)
+    }
+    if(!updateBlacklist(guild.id, blist.concat(toAdd))){
         return message.reply('Some error occurred. Please try again!').catch(console.error)
     }
     const embed = new MessageEmbed()
                         .setTitle(`Blacklisted Words Added for ${guild.name}`)
                         .setThumbnail(guild.iconURL({dynamic: true}))
                         .setDescription(
-                            words.length === 0 ?
+                            toAdd.length === 0 ?
                                 'No new blacklist words were added!' :
-                                `\`${words.join('\`, \`')}\``
+                                `\`${toAdd.join('\`, \`')}\``
                         ).setColor('RANDOM')
                         .setFooter(
                             `Requested by ${author.tag}`,
@@ -54,17 +58,22 @@ const addWords = async (message, words) => {
 const delWords = async (message, words) => {
     const {guild, channel, author} = message
     const blist = await getBlacklist(guild.id)
-    words = words.filter(w => blist.indexOf(w) >= 0) //Remove words not in blacklist (only needed for output)
-    if(!updateBlacklist(guild.id, blist.filter(w => words.indexOf(w) < 0))){
+    const toDel = []
+    for(const word of words){
+        if(blist.includes(word) && !toDel.includes(word)){
+            toDel.push(word)
+        }
+    }
+    if(!updateBlacklist(guild.id, blist.filter(w => !toDel.includes(w)))){
         return message.reply('Some error occurred. Please try again!').catch(console.error)
     }
     const embed = new MessageEmbed()
                         .setTitle(`Blacklisted Words Removed for ${guild.name}`)
                         .setThumbnail(guild.iconURL({dynamic: true}))
                         .setDescription(
-                            words.length === 0 ?
+                            toDel.length === 0 ?
                                 'No blacklisted words were removed!' :
-                                `\`${words.join('\`, \`')}\``
+                                `\`${toDel.join('\`, \`')}\``
                         ).setColor('RANDOM')
                         .setFooter(
                             `Requested by ${author.tag}`,
@@ -102,7 +111,7 @@ module.exports = {
     name: 'blacklist',
     desc: 'Add/delete/view blacklisted words for the server. Messages with blacklisted words will automatically be deleted!',
     expectedArgs: '<query> [<options>]',
-    parameters: '\`<query>\`: The query should be one of \`add\`, \`delete\`, \`view\`. By default the query will be taken as \`view\`.\n\`[<options>]\`: The options for the particular query.\n•__add__: Space separated list of words to add to blacklist. Words are NOT case-sensitive and repeated words will only be added once.\n•__delete__: Space separated list of words to be removed from blacklist. Words not present will not be removed.\n•__view__: No options needed!',
+    parameters: '\`<query>\`: The query should be one of \`add\`, \`delete\`, \`view\`. By default the query will be taken as \`view\`.\n\`[<options>]\`: The options for the particular query.\n•__add__: Space separated list of words to add to blacklist. Words are NOT case-sensitive and repeated words will only be added once.\n•__delete__: Space separated list of words to be removed from blacklist. Words not present will not be removed.\n•__view__: No options needed!\n\n**Note**: To add phrases as blacklisted, write them enclosed within double quotes',
     requiredPermissions: ['MANAGE_GUILD'],
     category: 'setting',
     /**
